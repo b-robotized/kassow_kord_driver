ea496416d09272bf184d59c0e675a75ef07aca8e
Yara Shahin <yara.shahin@b-robotized.com>
2025-12-22 16:27:16 +0100

    Fix the scripts.

--- kassow_kord_bringup/scripts/activate_controllers.sh
+++ kassow_kord_bringup/scripts/activate_controllers.sh
@@ -5,5 +5,5 @@ ros2 run controller_manager spawner -p scenario_controllers.yaml -c /b_controlle
 ros2 run controller_manager spawner -p scenario_controllers.yaml -c /b_controlled_box_cm kassow_left_joint_trajectory_controller --inactive
 ros2 run controller_manager spawner -p scenario_controllers.yaml -c /b_controlled_box_cm kassow_right_joint_trajectory_controller --inactive
 
-ros2 control switch_controller -c /b_controlled_box_cm --start-controllers kassow_left_joint_trajectory_controller
-ros2 control switch_controller -c /b_controlled_box_cm --start-controllers kassow_right_joint_trajectory_controller
+ros2 control switch_controllers -c /b_controlled_box_cm --activate kassow_left_joint_trajectory_controller
+ros2 control switch_controllers -c /b_controlled_box_cm --activate kassow_right_joint_trajectory_controller
--- kassow_kord_bringup/scripts/activate_kassow_robot.bash
+++ kassow_kord_bringup/scripts/activate_kassow_robot.bash
@@ -3,10 +3,11 @@
 ros2 run controller_manager spawner -p scenario_controllers.yaml -c /b_controlled_box_cm joint_state_broadcaster --inactive
 
 ros2 control set_hardware_component_state -c /b_controlled_box_cm kassow inactive
-ros2 control switch_controller -c /b_controlled_box_cm --start-controllers joint_state_broadcaster
+ros2 control switch_controllers -c /b_controlled_box_cm --activate joint_state_broadcaster
 
 ros2 control set_hardware_component_state -c /b_controlled_box_cm kassow active
 
 
 ros2 run controller_manager spawner -p scenario_controllers.yaml -c /b_controlled_box_cm kassow_joint_trajectory_controller --inactive
-ros2 control switch_controller -c /b_controlled_box_cm --start-controllers kassow_joint_trajectory_controller
+ros2 control switch_controllers -c /b_controlled_box_cm --activate kassow_joint_trajectory_controller
+
