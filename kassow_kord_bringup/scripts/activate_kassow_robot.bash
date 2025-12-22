989f2eb1dd817bd1ea7c0079d07d5287dc8352a9
Yara Shahin <yara.shahin@b-robotized.com>
2025-12-22 13:30:29 +0100

    Update the configurations.

--- kassow_kord_bringup/scripts/activate_kassow_robot.bash
+++ kassow_kord_bringup/scripts/activate_kassow_robot.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+ros2 run controller_manager spawner -p scenario_controllers.yaml -c /b_controlled_box_cm joint_state_broadcaster --inactive
+
 ros2 control set_hardware_component_state -c /b_controlled_box_cm kassow inactive
+ros2 control switch_controller -c /b_controlled_box_cm --start-controllers joint_state_broadcaster
+
 ros2 control set_hardware_component_state -c /b_controlled_box_cm kassow active
+
 
 ros2 run controller_manager spawner -p scenario_controllers.yaml -c /b_controlled_box_cm kassow_joint_trajectory_controller --inactive
 ros2 control switch_controller -c /b_controlled_box_cm --start-controllers kassow_joint_trajectory_controller
