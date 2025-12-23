4623242b068ca47887f349da5e5b4f47aa60b5fb
Yara Shahin <yara.shahin@b-robotized.com>
2025-12-23 14:20:26 +0200

    fix precommit after rebase

--- kassow_kord_bringup/scripts/activate_kassow_robot.bash
+++ kassow_kord_bringup/scripts/activate_kassow_robot.bash
@@ -10,4 +10,3 @@ ros2 control set_hardware_component_state -c /b_controlled_box_cm kassow active
 
 ros2 run controller_manager spawner -p scenario_controllers.yaml -c /b_controlled_box_cm kassow_joint_trajectory_controller --inactive
 ros2 control switch_controllers -c /b_controlled_box_cm --activate kassow_joint_trajectory_controller
-
